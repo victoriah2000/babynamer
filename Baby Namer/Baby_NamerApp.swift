9592fcc6723ae41d9244eefdcaa082a131b56810
Victoria Heric <victoriah_2000@yahoo.com>
2020-08-13 17:39:56 -0700

    A project is born.

---
+++ Baby Namer/Baby_NamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_NamerApp.swift
+//  Baby Namer
+//
+//  Created by Victoria Heric on 8/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Baby_NamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baby Namer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baby Namer
+//
+//  Created by Victoria Heric on 8/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
